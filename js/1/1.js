be41cbdace781bcf806ef4455da4e850e97f89b1
Arce <113393688+DevArce@users.noreply.github.com>
2024-09-27 14:32:31 -0700

    Solution and tests cases of problem 1 in javascript

---
+++ js/1/1.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+    for (let i = 0; i < nums.length; i++) {
+        const diff = target - nums[i];
+        const diffIndex = nums.slice(i+1).indexOf(diff) + i + 1;
+
+        if (diffIndex !== -1 && diffIndex !== i) {
+            return [i, diffIndex];
+        }
+    }
+};
+
+//twoSum([3, 3], 6)
+
+module.exports = twoSum;
