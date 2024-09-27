4834e087913f9f47443836947667e9e8ba7bf827
Arce <113393688+DevArce@users.noreply.github.com>
2024-09-27 12:52:12 -0700

    Test cases for problem 139

--- tests/139/139.test.js
+++ tests/139/139.test.js
@@ -1,7 +1,32 @@
-const wordBreak = require('../139/139.js');
+const wordBreak = require("../../problems/139/139");
 
-test('test case 1' , () => {
-    const s = "leetcode";
-    const wordDict = ["leet", "code"];
-    expect(wordBreak(s,wordDict)).toBe(true);
+test("test case 1", () => {
+  const s = "leetcode";
+  const wordDict = ["leet", "code"];
+  expect(wordBreak(s, wordDict)).toBe(true);
+});
+
+test("test case 2", () => {
+  const s = "applepenapple";
+  const wordDict = ["apple", "pen"];
+  expect(wordBreak(s, wordDict)).toBe(true);
+});
+
+test("test case 3", () => {
+  const s =
+    "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
+  const wordDict = [
+    "a",
+    "aa",
+    "aaa",
+    "aaaa",
+    "aaaaa",
+    "aaaaaa",
+    "aaaaaaa",
+    "aaaaaaaa",
+    "aaaaaaaaa",
+    "aaaaaaaaaa",
+  ];
+
+  expect(wordBreak(s, wordDict)).toBe(false);
 });
